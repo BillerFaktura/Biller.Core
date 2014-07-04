a98768c3c7eaed16295c09083883e52043247425
Igor Lückel <igor.lueckel@googlemail.com>
2014-07-04 13:15:10 +0200

    [UpdateManager] Added Models

---
+++ Models/UpdateModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biller.Core.Models
+{
+    public class UpdateModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string UpdateURL { get; set; }
+        public double Version { get; set; }
+        public List<UpdateRequireModel> RequiredApps { get; set; }
+    }
+
+    public class UpdateModels : ObservableCollection<UpdateModel> { }
+}
